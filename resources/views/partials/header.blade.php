d1bce95c0d778ed00fd4a463fa7d02cedf8a2623
glcordon <glcordon@gmail.com>
2019-03-05 00:23:18 -0500

    updated client view page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -12,9 +12,17 @@
           <li class="nav-item">
             <a class="nav-link" href="/client">View Clients</a>
           </li>
-	<li class="nav-item">
-		<a class="nav-link" href="/client/{{ \Auth::user()->id }}">My Caseload</a>
-	</li>
+          <li class="nav-item">
+            <a class="nav-link" href="/client/{{ \Auth::user()->id }}">My Caseload</a>
+          </li>
+          <li class="nav-item">
+              <form action="http://157.230.6.170/admin/logout" method="POST">
+                <input type="hidden" name="_token" value="vbsDKFY3gKdOeJGTC4QuszmcxQZL2bX4yyF5cxbP">
+                <button type="submit" class="btn btn-danger btn-block">
+                                                    <i class="voyager-power"></i>
+                                                    Logout
+    d
+            </li>
         </ul>
         <form class="form-inline mt-2 mt-md-0">
           <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
