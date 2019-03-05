f8ef1dad017814b2c28d71bb1cca0190c059363b
glcordon <glcordon@gmail.com>
2019-03-04 23:32:11 -0500

    updated client view page

--- resources/views/partials/clients/client-add.blade.php
+++ resources/views/partials/clients/client-add.blade.php
@@ -107,7 +107,7 @@
                                 <option value="complete">Complete</option>
                             </select>
                         </li>
-        <ul class="list-group">
+        <ul class="list-group  list-group-flush">
             @foreach($services as $service)
                 <li class="list-group-item"><input type="checkbox" name="service" value="{{ $service->id }}" id="service_{{ $service->id }}"> <label for="service_{{ $service->id }}">{{ $service->service_name }}</label></li>
             @endforeach
