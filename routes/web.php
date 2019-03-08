8535d836373328f43ac58eb7e155f854357c7141
glcordon <glcordon@gmail.com>
2019-03-07 23:10:51 -0500

    Added login page to welcome page

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Auth::routes();
 Route::get('/', function(){return view('welcome');});
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
