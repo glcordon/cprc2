6bf3d2080917b5e2e89659a19d1c5973f69fca57
glcordon <glcordon@gmail.com>
2019-03-07 22:30:12 -0500

    Added login page to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,7 +67,7 @@
                                         Login
                                     </button>
      
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{-- <a class="btn btn-link" href="{{ route('password.request') }}"> --}}
                                         Forgot Your Password?
                                     </a>
                                 </div>
