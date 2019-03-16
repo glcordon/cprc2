edb94dfd37331426306267dafd0c7fb167ee2078
glcordon <glcordon@gmail.com>
2019-03-15 22:33:41 -0400

    Added Datatables

--- resources/views/partials/clients/client-index.blade.php
+++ resources/views/partials/clients/client-index.blade.php
@@ -12,51 +12,53 @@
        </div>
             <div class="row">
 
+            <div class="col-12">
+                    <table class="table table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Client Name</th>
+                                    <th>Services</th>
+                                    <th>Assigned To</th>
+                                    <th>Date Assigned</th>
+                                    <th><a href="/client-add" class="btn btn-primary">Add New</a> </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($clients as $client)
+                                    <tr>
+                                        <td>{{ $client->last_name ?? ''}}, {{ $client->first_name ?? ''}}</td>
+                                        <td>
+                                            {{-- {{ dump($client->services) }} --}}
+                                            @if(isset($client->services))
+                                                @foreach ($client->services as $service)
+                                                    <small><em>{{ $service->service_name }}</em></small><br />
+                                                @endforeach
+                                            @endif
             
-            <table class="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>Client Name</th>
-                        <th>Services</th>
-                        <th>Assigned To</th>
-                        <th>Date Assigned</th>
-                        <th><a href="/client-add" class="btn btn-primary">Add New</a> </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($clients as $client)
-                        <tr>
-                            <td>{{ $client->last_name ?? ''}}, {{ $client->first_name ?? ''}}</td>
-                            <td>
-                                {{-- {{ dump($client->services) }} --}}
-                                @if(isset($client->services))
-                                    @foreach ($client->services as $service)
-                                        <small><em>{{ $service->service_name }}</em></small><br />
-                                    @endforeach
-                                @endif
-
-                            </td>
-                            <td>{{ $client->assignedTo->name ?? 'Not Assigned'}}
-                                {{--  @if(!$client->assignedTo)
-                                    <a href="#" id="assign_caseworker" class="btn btn-default">Assign Case Worker</a>
-                                @endif  --}}
-                            </td>
-                            <td>{{ $client->assignedTo->updated_at ?? ' - ' }}</td>
-                            <td><a href="/client/contact/{{ $client->id }}" class="btn-success btn">Touch</a>
-                                    
-                                    <a href="#" class="btn btn-default">Edit</a>
-                                    {{--  <button class="btn btn-primary"> View Notes</button>  --}}
-                                    <a href="/delete-client/{{ $client->id }}" id="delete" class="btn btn-danger"> <span class="glyphicon glyphicon-remove"><strong> X </strong></span></a>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td colspan="4">{{ $clients->links() }}</td>
-                    </tr>
-                </tfoot>
-            </table>
+                                        </td>
+                                        <td>{{ $client->assignedTo->name ?? 'Not Assigned'}}
+                                            {{--  @if(!$client->assignedTo)
+                                                <a href="#" id="assign_caseworker" class="btn btn-default">Assign Case Worker</a>
+                                            @endif  --}}
+                                        </td>
+                                        <td>{{ $client->assignedTo->updated_at ?? ' - ' }}</td>
+                                        <td><a href="/client/contact/{{ $client->id }}" class="btn-success btn">Touch</a>
+                                                
+                                                <a href="#" class="btn btn-default">Edit</a>
+                                                {{--  <button class="btn btn-primary"> View Notes</button>  --}}
+                                                <a href="/delete-client/{{ $client->id }}" id="delete" class="btn btn-danger"> <span class="glyphicon glyphicon-remove"><strong> X </strong></span></a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <td colspan="4">{{ $clients->links() }}</td>
+                                </tr>
+                            </tfoot>
+                        </table>
+            </div>
+           
   
 </div>
 
