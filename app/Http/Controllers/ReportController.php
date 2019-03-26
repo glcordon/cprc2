21654efd27d53fbf3c8d9c5df5aa2b8d110c2666
glcordon <glcordon@gmail.com>
2019-03-25 23:04:47 -0400

    Barry PDF

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -12,7 +12,7 @@ class ReportController extends Controller
     public function index(Request $request)
     {
         $data = ['title' => 'Welcome to HDTuto.com'];
-        $pdf = PDF::loadView('welcome', $data);
+        $pdf = PDF::loadView('pdf', $data);
   
         return $pdf->download('itsolutionstuff.pdf');
     }
