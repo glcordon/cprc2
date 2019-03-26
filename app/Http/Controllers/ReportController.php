52f303145e706c47486cbb66847d23e981a0ff79
glcordon <glcordon@gmail.com>
2019-03-25 23:41:04 -0400

    Barry PDF

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+use App\Client;
 use PDF;
 
 class ReportController extends Controller
@@ -13,6 +14,9 @@ class ReportController extends Controller
     public function index(Request $request)
     {
         $today = Carbon::now();
+        $clients = Client::get();
+        $totalActive = $clients->where('status', '1')->count();
+        dd($clients->services->where('service_name', 'Education'));
         $data = ['title' => 'Welcome to HDTuto.com', 'today' => $today];
         $pdf = PDF::loadView('make_pdf', $data);
   
