22036ba896bbed7a94231251089829ded51a149c
glcordon <glcordon@gmail.com>
2019-03-25 23:21:42 -0400

    Barry PDF

--- resources/views/make_pdf.blade.php
+++ resources/views/make_pdf.blade.php
@@ -10,8 +10,39 @@
 		<h1>North Carolina Local Reentry Council</h1>
 		<h2>Monthly Progress Report</h2>
 		<h3>Due by 15th of each Month</h3>
-		
-		
+<table>
+	<tr>
+		<td>Intermediary Agency:</td>
+		<td>CRAVEN PAMLICO</td>
+		<td>Reporting Month/Year:</td>
+		<td></td>
+	</tr>
+	<tr>
+		<td colspan="4">Activity Summary</td>
+	</tr>
+	<tr>
+		<td colspan="4">Please enter the number of reentry clients for each category during the reporting period.</td>
+	</tr>
+	<tr>
+		<td>Enrolled</td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td>Number of reentry clients:</td>
+		<td></td>
+		<td>Active</td>
+		<td></td>
+	</tr>
+						
+</table>
+
+
+
+
+
+
 	
 </body>
 </html>
