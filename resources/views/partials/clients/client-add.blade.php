c18df6c7752a546049d601abf0a754f2571b920f
glcordon <glcordon@gmail.com>
2019-03-26 20:40:30 -0400

    Updated Forms

--- resources/views/partials/clients/client-add.blade.php
+++ resources/views/partials/clients/client-add.blade.php
@@ -58,8 +58,8 @@
                 <label class="gfield_label" for="input_4_2">Suffix<span class="gfield_required"></span></label>
                 <select name="suffix" id="suffix" class="form-control">
                     <option value="">Select One</option>
-                    <option value="Sr" {{ isset($client) ? ($client->maritial_status == 'Sr' ? 'selected="selected"' : '') : ''}}>Sr</option>
                     <option value="Jr" {{ isset($client) ? ($client->maritial_status == 'Jr' ? 'selected="selected"' : '') : ''}}>Jr</option>
+                    <option value="Sr" {{ isset($client) ? ($client->maritial_status == 'Sr' ? 'selected="selected"' : '') : ''}}>Sr</option>
                     <option value="III" {{ isset($client) ? ($client->maritial_status == 'III' ? 'selected="selected"' : '') : ''}}>III</option>
                     <option value="IV" {{ isset($client) ? ($client->maritial_status == 'IV' ? 'selected="selected"' : '') : ''}}>IV</option>
                 </select>
