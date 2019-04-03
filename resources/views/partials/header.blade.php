0ac70260a5b9a317c8da48cde5302bf5e78a113a
glcordon <glcordon@gmail.com>
2019-04-03 19:11:27 -0400

    added status

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -58,9 +58,21 @@
           <label for="note_date">Select Month and Year</label>
           <select name="month" id="month" class="form-control">
             <option value="">Select Month</option>
-            @for($i=1;$i<=12;$i++)
+            {{--  @for($i=1;$i<=12;$i++)
             <option value="{{ $i }}">{{ $i }}</option>
-            @endfor
+            @endfor  --}}
+            <option value="01">January</option>
+            <option value="02">February</option>
+            <option value="03">March</option>
+            <option value="04">April</option>
+            <option value="05">May</option>
+            <option value="06">June</option>
+            <option value="07">July</option>
+            <option value="08s">August</option>
+            <option value="09">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
           </select>
           <select name="year" id="year" class="form-control">
             <option value="">Select Year</option>
