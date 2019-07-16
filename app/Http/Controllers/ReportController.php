93331e5286acc792df44dce9260f039ab3a302b4
glcordon <glcordon@gmail.com>
2019-07-15 23:11:46 -0400

    updating laravel to the newest

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -30,7 +30,7 @@ class ReportController extends Controller
             }));
        });
        dd($numberOfServices->filter(function($service, $key){
-         return [$service];
+         return [$key];
        })->toArray());
         $service = Service::get();
         // $data = ['today' => $today,'thisDate' =>$thisDate, 'service' => $service, 'totalActive' => $totalActive, 'all' => $clients->all()];
