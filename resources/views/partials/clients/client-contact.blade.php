705b2caee3be8ebf7782a9f7caaf46528cc9a0a5
glcordon <glcordon@gmail.com>
2019-07-16 19:00:41 -0400

    added id to job delete text

--- resources/views/partials/clients/client-contact.blade.php
+++ resources/views/partials/clients/client-contact.blade.php
@@ -51,7 +51,7 @@
           <input type="hidden" name="this_id" value="{{ $clients->id }}">
          <img class="round" width="150" height="150" avatar="{{ $clients->first_name ?? ''}} {{ $clients->last_name ?? ''}}">
          <a href="/client/{{ $clients->id }}/edit" class="btn btn-default">Edit/View</a>
-        <div class="card-title success"><h1>{{ $clients->last_name ?? ''}}, {{ $clients->first_name ?? ''}}</h1></div>
+        <div class="card-title success" style="text-align:center"><h1>{{ $clients->last_name ?? ''}}, {{ $clients->first_name ?? ''}}</h1></div>
         <div class="card-body">
             <div class="card-text"> 
                     {{ $clients->address_1 }} <br>
