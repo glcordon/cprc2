07ef3d2961fbf296c9112cdc2cbcfd7912fd8596
glcordon <glcordon@gmail.com>
2019-07-16 17:27:53 -0400

    added id to job delete text

--- resources/views/partials/clients/client-contact.blade.php
+++ resources/views/partials/clients/client-contact.blade.php
@@ -94,7 +94,7 @@
               @if(\Carbon\Carbon::now()->diffInDays($clients->enrollment_date) > $srv->service_duration)
                 Expired
               @else 
-                {{ $clients->enrollment_date->addDays(\Carbon\Carbon::now()->diffInDays($clients->enrollment_date)) }}
+                {{ Carbon\Carbon::parse($clients->enrollment_date)->addDays(\Carbon\Carbon::now()->diffInDays($clients->enrollment_date)) }}
               @endif 
             </div>
             @endforeach
