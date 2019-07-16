e0125eb585aae61df137302eadb83d5293ee4b28
glcordon <glcordon@gmail.com>
2019-07-15 23:23:05 -0400

    updating laravel to the newest

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -27,7 +27,7 @@ class ReportController extends Controller
         $count = 0;
        foreach($activeClients as $ac)
        { 
-           dd($ac->services->flatten(1));
+           dd($ac->services->toArray());
            $xo = collect([]);
            foreach($ac->services->groupBy('service_name') as $key => $serv){
                dd($serv);
