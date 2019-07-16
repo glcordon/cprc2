d95c188d4a3555d8474395be70cd9a28e532b17a
glcordon <glcordon@gmail.com>
2019-07-15 22:07:45 -0400

    updating laravel to the newest

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -30,7 +30,7 @@ class ReportController extends Controller
             $numberOfServices->push($key);
            } 
        } 
-       dd($numberOfServices);
+       dd($numberOfServices->toArray());
         $service = Service::get();
         // $data = ['today' => $today,'thisDate' =>$thisDate, 'service' => $service, 'totalActive' => $totalActive, 'all' => $clients->all()];
         return view('make_pdf', compact('clients', 'totalActive', 'all', 'service', 'thisDate'));
