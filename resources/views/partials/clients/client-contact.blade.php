b112df5bfd8f429749a2efa4b98f22daf79fc44d
glcordon <glcordon@gmail.com>
2019-07-16 11:42:56 -0400

    added job modal to client contact

--- resources/views/partials/clients/client-contact.blade.php
+++ resources/views/partials/clients/client-contact.blade.php
@@ -356,7 +356,7 @@
                   job_address: job_address}
               })
               .done(function(data){
-                $(data['job_name'] + ' | <div id="delete" class="btn btn-sm danger" style="font-weight:900; color:red">X</div> <br>
+                $(data['job_name'] + ' | <div id="delete" id="'+data['id']+'" class="btn btn-sm danger" style="font-weight:900; color:red">X</div> <br>
                 ' + data['job_address'] +' <br>
                 <small><em> Salary Code: ' + data['salary'] +' </em></small> <br>
               <small><em> Start Date:' + data['start_date'] +'</em></small> <br>
