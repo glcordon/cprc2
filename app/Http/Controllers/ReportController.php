add644d97706e30754dbc3f695c365bf23857a1c
glcordon <glcordon@gmail.com>
2019-07-15 22:42:04 -0400

    updating laravel to the newest

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -30,7 +30,7 @@ class ReportController extends Controller
             }));
        });
           
-       dd($numberOfServices);
+       dd($numberOfServices->flatten(1));
         $service = Service::get();
         // $data = ['today' => $today,'thisDate' =>$thisDate, 'service' => $service, 'totalActive' => $totalActive, 'all' => $clients->all()];
         return view('make_pdf', compact('clients', 'totalActive', 'all', 'service', 'thisDate'));
