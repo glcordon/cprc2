396a3c28447d42778dc720a88462a75e2c612d52
glcordon <glcordon@gmail.com>
2019-07-16 22:10:28 -0400

    Added duration to touch point

--- resources/views/partials/clients/client-contact.blade.php
+++ resources/views/partials/clients/client-contact.blade.php
@@ -16,7 +16,7 @@
             left: 29px;
             width: 2px;
             height: 100%;
-            z-index: 400;
+            z-index: 10;
         }
         ul.timeline > li {
             margin: 20px 0;
@@ -32,7 +32,7 @@
             left: 20px;
             width: 20px;
             height: 20px;
-            z-index: 400;
+            z-index: 10;
         }
         a#title_type
         {
