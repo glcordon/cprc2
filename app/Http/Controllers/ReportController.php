6676e9e6b9a3ce55cc5adc426d7364e51b746e47
glcordon <glcordon@gmail.com>
2019-07-16 21:04:18 -0400

    Updated model for jobs relationship

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -46,7 +46,7 @@ class ReportController extends Controller
            } 
            
        } 
-       dd($inactiveClients->groupBy('status')); 
+       dd($inactiveClients->pluck('status')); 
        $jobCount = array_count_values($numberOfJobs->sort()->toArray());
        $serviceCount = array_count_values($numberOfServices->sort()->toArray());
         $service = Service::get();
