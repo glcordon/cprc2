7e1eda6fc201f703302beb20678eeb183adb455a
glcordon <glcordon@gmail.com>
2019-07-17 09:06:11 -0400

    added dashboard route

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -19,6 +19,39 @@
 
 		</div>
 	</div>
+	<div class="row">
+			<div class="col-md-3">
+			  <div class="card-counter primary">
+				<i class="fa fa-code-fork"></i>
+				<span class="count-numbers">12</span>
+				<span class="count-name">Flowz</span>
+			  </div>
+			</div>
+		
+			<div class="col-md-3">
+			  <div class="card-counter danger">
+				<i class="fa fa-ticket"></i>
+				<span class="count-numbers">599</span>
+				<span class="count-name">Instances</span>
+			  </div>
+			</div>
+		
+			<div class="col-md-3">
+			  <div class="card-counter success">
+				<i class="fa fa-database"></i>
+				<span class="count-numbers">6875</span>
+				<span class="count-name">Data</span>
+			  </div>
+			</div>
+		
+			<div class="col-md-3">
+			  <div class="card-counter info">
+				<i class="fa fa-users"></i>
+				<span class="count-numbers">35</span>
+				<span class="count-name">Users</span>
+			  </div>
+			</div>
+		  </div>
 		<div class="row" style="background-color:#b3cde0; color:black">
 			<div class="col-3">Enrolled:</div>
 			<div  style="border:1px solid black;" class="col-3">{{ count($all) }}</div>
