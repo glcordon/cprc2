abf51521e7c2ed59fc18f5ecf844cb3d879ac1dc
glcordon <glcordon@gmail.com>
2019-07-16 20:14:44 -0400

    Updated model for jobs relationship

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -44,10 +44,9 @@ class ReportController extends Controller
        } 
        $jobCount = array_count_values($numberOfJobs->sort()->toArray());
        $serviceCount = array_count_values($numberOfServices->sort()->toArray());
-       dd($jobCount);
         $service = Service::get();
         // $data = ['today' => $today,'thisDate' =>$thisDate, 'service' => $service, 'totalActive' => $totalActive, 'all' => $clients->all()];
-        return view('make_pdf', compact('clients', 'totalActive', 'all', 'service', 'inactiveClients', 'serviceCount', 'thisDate'));
+        return view('make_pdf', compact('clients', 'totalActive', 'all', 'service','jobCount', 'inactiveClients', 'serviceCount', 'thisDate'));
         $pdf = PDF::loadView('make_pdf', $data);
         return $pdf->download('itsolutionstuff.pdf');
     }
