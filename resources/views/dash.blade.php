65fac07ae360cbc10b8e710a235c27a16d37c1fb
glcordon <glcordon@gmail.com>
2019-07-17 09:09:28 -0400

    added dashboard route

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -82,41 +82,21 @@
 			<div class="col-md-3">
 			  <div class="card-counter primary">
 				<i class="fa fa-code-fork"></i>
-				<span class="count-numbers">12</span>
-				<span class="count-name">Flowz</span>
-			  </div>
-			</div>
-		
-			<div class="col-md-3">
-			  <div class="card-counter danger">
-				<i class="fa fa-ticket"></i>
-				<span class="count-numbers">599</span>
-				<span class="count-name">Instances</span>
+				<span class="count-numbers">{{ count($all) }}<</span>
+				<span class="count-name">Enrolled This Month</span>
 			  </div>
 			</div>
 		
 			<div class="col-md-3">
 			  <div class="card-counter success">
 				<i class="fa fa-database"></i>
-				<span class="count-numbers">6875</span>
-				<span class="count-name">Data</span>
+				<span class="count-numbers">{{ $totalActive }}</span>
+				<span class="count-name">Currently Active</span>
 			  </div>
 			</div>
 		
-			<div class="col-md-3">
-			  <div class="card-counter info">
-				<i class="fa fa-users"></i>
-				<span class="count-numbers">35</span>
-				<span class="count-name">Users</span>
-			  </div>
-			</div>
+			
 		  </div>
-		<div class="row" style="background-color:#b3cde0; color:black">
-			<div class="col-3">Enrolled:</div>
-			<div  style="border:1px solid black;" class="col-3">{{ count($all) }}</div>
-			<div class="col-3">Active</div>
-			<div style="border:1px solid black" class="col-3">{{ $totalActive }}</div>
-		</div>
 		
 			<div class="row">
 				<div class="col-4">
