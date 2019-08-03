16f7c5ac2f07bfe9cc6c7c6fe0253fae48acaf92
glcordon <glcordon@gmail.com>
2019-08-03 10:06:54 -0400

    Upload

--- app/Imports/ClientImport.php
+++ app/Imports/ClientImport.php
@@ -16,7 +16,7 @@ class ClientImport implements ToModel, WithHeadingRow
     public function model(array $row)
     {
 
-        dd($row->enrollment_date->format('Y-m-d'));
+        dd($row['enrollment_date']->format('Y-m-d'));
         return new Client([
             //
         "first_name" => explode(' ', $row['name'])[0] ?? '',
