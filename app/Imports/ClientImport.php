fdd3fbde8b1648a41c05c52d4f9644fac2b2218c
glcordon <glcordon@gmail.com>
2019-08-03 10:13:00 -0400

    Upload

--- app/Imports/ClientImport.php
+++ app/Imports/ClientImport.php
@@ -15,8 +15,7 @@ class ClientImport implements ToModel, WithHeadingRow
     */
     public function model(array $row)
     {
-
-        dd(\Carbon\Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['dob'])));
+        dd($row);
         return new Client([
             //
         "first_name" => explode(' ', $row['name'])[0] ?? '',
@@ -47,7 +46,7 @@ class ClientImport implements ToModel, WithHeadingRow
         "race" => $row['race'] ?? '',
         "ethnicity" => $row['ethnicity'] ?? '',
         "education" => $row['level_of_education'] ?? '',
-        "dob" => Carbon\Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['dob'])) ?? '',
+        "dob" => \Carbon\Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['dob'])) ?? '',
         // "supervisors_name" => $row[''] ?? '',
         // "supervisors_phone" => $row[''] ?? '',
         // "supervisors_email" => $row[''] ?? '',
@@ -58,7 +57,7 @@ class ClientImport implements ToModel, WithHeadingRow
         // "released_from" => $row[''] ?? '',
         // "under_supervision" => $row[''] ?? '',
         // "middle_name" => $row[''] ?? '',
-        "enrollment_date" => Carbon\Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['enrollment_date'])) ?? '',
+        "enrollment_date" => \Carbon\Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['enrollment_date'])) ?? '',
         "suffix" => explode(' ', $row['name'])[1] ?? '',
         "charge" => 'testing',
         // "risk_level" => $row[''] ?? '',
