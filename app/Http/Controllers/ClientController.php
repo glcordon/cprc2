fb0c1473ff40cf16a01fbf8e2a758e9c8b659327
glcordon <glcordon@gmail.com>
2019-08-04 22:25:41 -0400

    working on excel dates

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -308,9 +308,9 @@ class ClientController extends Controller
 
         $path = $request->file('excel_file')->store('csvs');
         
-        $hello = (new ClientImport)->toCollection($path, 'local', \Maatwebsite\Excel\Excel::XLSX);
+        $hello = (new ClientImport)->toCollection($path, 'local', \Maatwebsite\Excel\Excel::XLSX)->flatten(1)->toArray();
 
-        return($hello->flatten(1)->toArray());
+        return($hello);
         return Client::where("assigned_to", "14")->get();
     }
 }
