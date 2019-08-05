78661221bf346c8172c0a3a60d94652ade455f0e
glcordon <glcordon@gmail.com>
2019-08-04 22:51:07 -0400

    working on excel dates

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -308,9 +308,49 @@ class ClientController extends Controller
 
         $path = $request->file('excel_file')->store('csvs');
         
-        $client = (new ClientImport)->toCollection($path, 'local', \Maatwebsite\Excel\Excel::XLSX)->flatten(1)->toArray();
+        $clients = (new ClientImport)->toCollection($path, 'local', \Maatwebsite\Excel\Excel::XLSX)->flatten(1)->toArray();
 
-        return(collect($client)->map(function($item){
+        foreach($clients as $row)
+        {
+            Client::create([
+                "first_name" => explode(' ', $row['name'])[0] ?? '',
+                "last_name" => explode(' ', $row['name'])[1] ?? '',
+                "address_1" =>  explode(',', $row['address'])[0] ?? '',
+                "city" => explode(',', $row['address'])[1] ?? '',
+                "zip" => explode(',', $row['address'])[2] ?? '',
+                "email_address" => $row['email'] ?? '',
+                "primary_phone" => $row['telephone'] ?? '',
+                "secondary_phone" => $row['telephone_2'] ?? '',
+                "is_active" => 1 ?? '',
+                "created_at" => \Carbon\Carbon::now() ?? '',
+                "updated_at" => \Carbon\Carbon::now() ?? '',
+                "deleted_at" => null,
+                "case_worker" => '',
+                "assigned_to" => $row['assigned_to'] ?? '14',
+                "user_id" => null,
+                "address_2" => $row['address_2'] ?? '',
+                "sex" => $row['gender'][0] ?? '',
+                "release_date" => $row['release_date'] ?? '',
+                "status" => $row['status'] ?? '',
+                "state" => $row['state'] ?? '',
+                "full_name" => $row['name'] ?? '',
+                "citizenship" => $row['citizenship'] ?? '',
+                "form_of_id" => [],
+                "ncdps_id" => $row['opus'] ?? '',
+                "maritial_status" => $row['marital_status'] ?? '',
+                "race" => $row['race'] ?? '',
+                "ethnicity" => $row['ethnicity'] ?? '',
+                "education" => $row['level_of_education'] ?? '',
+                "dob" => $this->convertExcelDate($row['dob']) ?? '',
+                "enrollment_date" => $this->convertExcelDate($row['enrollment_date']) ?? '',
+                "suffix" => explode(' ', $row['name'])[1] ?? '',
+                "charge" => 'testing',
+                "first_offence_age" => $row['age_at_1stoffense'] ?? '',
+                "number_of_priors" => $row['of_priors'] ?? ''
+            ]);
+        }
+
+        return(collect($clients)->map(function($item){
             return $item;
                 return $this->convertExcelDate($item['dob']);
            
