eb56bb92c888ec68819b5e1519dd59b9b3bfc0ba
glcordon <glcordon@gmail.com>
2019-10-16 19:20:44 -0400

    added report route

--- routes/web.php
+++ routes/web.php
@@ -41,5 +41,5 @@ Route::post('/add-service', 'ClientController@addService');
 Route::get('/report-generate', 'ReportController@index')->name('pdf');
 Route::post('/report-generate', 'ReportController@index')->name('pdf_post');
 Route::post('/participation-report', 'ReportController@participantReport')->name('participation_report');
-
+route::get('/ap/report', 'AccountsPayableController@index');
 });
