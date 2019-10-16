aa137629c200775708b0d7d0ef4ac77a4482eca6
glcordon <glcordon@gmail.com>
2019-10-16 19:36:18 -0400

    added report route

--- app/Http/Controllers/AccountsPayableController.php
+++ app/Http/Controllers/AccountsPayableController.php
@@ -22,7 +22,7 @@ class AccountsPayableController extends Controller
         $thisDate = Carbon::parse($request->searchMonth);
         $clients = Client::whereHas('services', function ($query) use($today) {
             $query->whereMonth('client_service.created_at','=', Carbon::now()->subMonth()->month);
-        })->get();
+        })->with('services')->get();
         return $clients;
         $start = new Carbon('first day of this month');
         $clientsQuery = Client::whereMonth('enrollment_date','=', $thisDate->month)->whereYear('enrollment_date', '=', $thisDate->year);
