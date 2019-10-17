58828a67e51c92e90d3037ee5521c57c1628310f
glcordon <glcordon@gmail.com>
2019-10-16 22:14:15 -0400

    added view for ap report

--- resources/views/partials/ap/index.blade.php
+++ resources/views/partials/ap/index.blade.php
@@ -1,8 +1,8 @@
 @foreach($clientData as $client)
 {{ $client['first'] }}, {{ $client['last'] }} <br /> 
 @foreach($client['service'] as $cs)
-    {{ $cs->service_name }} <hr>
-    {{ $cs->pivot }}
+    {{ $cs['service_name'] }} <hr>
+    {{ $cs['pivot'] }}
 @endforeach
 <hr>
 @endforeach
