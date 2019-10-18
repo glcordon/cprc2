8f32730c9bdc738e46748bba283f41f8f04dd29a
glcordon <glcordon@gmail.com>
2019-10-18 19:42:22 -0400

    troubleshooting services section

--- resources/views/partials/clients/client-contact.blade.php
+++ resources/views/partials/clients/client-contact.blade.php
@@ -280,7 +280,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Add Touch Point</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Add Service</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -298,7 +298,7 @@
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary" id="save">Save changes</button>
+            <button type="button" class="btn btn-primary" id="saveService">Save changes</button>
           </div>
         </div>
       </div>
