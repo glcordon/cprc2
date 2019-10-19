15dca00fa53a194126082fc516913e647673fb64
glcordon <glcordon@gmail.com>
2019-10-19 17:23:25 -0400

    fixed error

--- resources/views/partials/ap/index.blade.php
+++ resources/views/partials/ap/index.blade.php
@@ -7,11 +7,11 @@
 </div>
 
 @foreach($clientData as $client)
-@dd($client)
 {{ $client['first'] }}, {{ $client['last'] }} <br /> 
 @foreach($client['service'] as $cs)
     {{ $cs['service_name'] }}
     <div class="form-group" style="padding:5px 0">
+        @dump($cs['pivot'])
       <label for="">$</label>
       <input type="text"
         class="form-control" name="authorized_price" id="authorized_price" value="{{ $cs['pivot']['authorized_price'] ?? '0.00'}}" aria-describedby="helpId" placeholder="">
