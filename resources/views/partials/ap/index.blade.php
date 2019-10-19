ff7733c97c4d5e8f5019c6b80b573c50f9219faf
glcordon <glcordon@gmail.com>
2019-10-19 17:11:36 -0400

    fixed error

--- resources/views/partials/ap/index.blade.php
+++ resources/views/partials/ap/index.blade.php
@@ -14,8 +14,6 @@
       <label for="">$</label>
       <input type="text"
         class="form-control" name="authorized_price" id="authorized_price" value="{{ $cs['pivot']['authorized_price'] ?? '0.00'}}" aria-describedby="helpId" placeholder="">
-    </div>
-    <div class="form-group">
       <label for="">Date Authorized</label>
       <input type="date"
         class="form-control" name="date_authorized" id="date_authorized" value="{{ $cs['pivot']['date_authorized'] ?? 'Not Authorized' }}" aria-describedby="helpId" placeholder="">
