2693f52d113fe85a2ad8c7fb41de593eb5cbfb1c
glcordon <glcordon@gmail.com>
2019-10-19 17:24:07 -0400

    fixed error

--- resources/views/partials/ap/index.blade.php
+++ resources/views/partials/ap/index.blade.php
@@ -11,7 +11,6 @@
 @foreach($client['service'] as $cs)
     {{ $cs['service_name'] }}
     <div class="form-group" style="padding:5px 0">
-        @dump($cs['pivot'])
       <label for="">$</label>
       <input type="text"
         class="form-control" name="authorized_price" id="authorized_price" value="{{ $cs['pivot']['authorized_price'] ?? '0.00'}}" aria-describedby="helpId" placeholder="">
