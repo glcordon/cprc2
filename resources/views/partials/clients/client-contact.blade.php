87e1cb194d5e8f5cdd7b5b3a92af11ea687de461
glcordon <glcordon@gmail.com>
2019-10-18 22:52:00 -0400

    troubleshooting services section

--- resources/views/partials/clients/client-contact.blade.php
+++ resources/views/partials/clients/client-contact.blade.php
@@ -427,7 +427,7 @@
 
         $('#saveServices').on('click', function(e){
             e.preventDefault();
-            console.log($(this).parent().parent().html())
+            console.log($(this).parent().html())
             return false;
             let service_id = $('select#service_name').val();
             var service_name = $('select#service_name  option:selected').text();
