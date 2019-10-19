2b1c8f973170995a138f2682c41a17afc16cbe9c
glcordon <glcordon@gmail.com>
2019-10-19 09:14:09 -0400

    troubleshooting services section

--- resources/views/partials/ap/index.blade.php
+++ resources/views/partials/ap/index.blade.php
@@ -2,9 +2,9 @@
 {{ $client['first'] }}, {{ $client['last'] }} <br /> 
 @foreach($client['service'] as $cs)
     {{ $cs['service_name'] }} -
-   {{ $cs['pivot']['authorized_price'] }}<br /> 
-   {{ $cs['pivot']['date_authorized'] }}<br /> 
-   {{ $cs['pivot']['notes'] }}<br /> 
+   {{ $cs['pivot']['authorized_price'] ?? ''}}<br /> 
+   {{ $cs['pivot']['date_authorized'] ?? '' }}<br /> 
+   {{ $cs['pivot']['notes']  ?? ''}}<br /> 
 @endforeach
 <hr>
 @endforeach
