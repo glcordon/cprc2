b7c12e13a0286d3268ffdaac76ccdfb47f1065a3
glcordon <glcordon@gmail.com>
2019-10-19 10:07:03 -0400

    troubleshooting services section

--- resources/views/partials/ap/index.blade.php
+++ resources/views/partials/ap/index.blade.php
@@ -2,9 +2,14 @@
 {{ $client['first'] }}, {{ $client['last'] }} <br /> 
 @foreach($client['service'] as $cs)
     {{ $cs['service_name'] }}
-    ${{ $cs['pivot']['authorized_price'] ?? '0.00'}}<br /> 
+    <div class="form-group">
+      <label for="">$</label>
+      <input type="text"
+        class="form-control" name="authorized_price" id="authorized_price" value="{{ $cs['pivot']['authorized_price'] ?? '0.00'}}" aria-describedby="helpId" placeholder="">
+      <small id="helpId" class="form-text text-muted">Help text</small>
+    </div>
+    $<br /> 
     {{ $cs['pivot']['date_authorized'] ?? 'Not Authorized' }}<br /> 
-    {{ $cs['pivot']['notes']  ?? 'No Notes'}}<br /> 
 @endforeach
 <hr>
 @endforeach
