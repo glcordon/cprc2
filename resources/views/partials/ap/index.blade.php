c7dc70afb3d547c097966ee241b87cba454e5f2f
glcordon <glcordon@gmail.com>
2019-10-19 09:09:18 -0400

    troubleshooting services section

--- resources/views/partials/ap/index.blade.php
+++ resources/views/partials/ap/index.blade.php
@@ -2,7 +2,6 @@
 {{ $client['first'] }}, {{ $client['last'] }} <br /> 
 @foreach($client['service'] as $cs)
     {{ $cs['service_name'] }} -
-    {{ $cs['service_price'] }} -
    {{ $cs['pivot']['updated_at'] }}<br /> 
 @endforeach
 <hr>
