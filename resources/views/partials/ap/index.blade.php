66e5f39439990de1f973fc48953b791e9470beee
glcordon <glcordon@gmail.com>
2019-10-19 10:10:05 -0400

    troubleshooting services section

--- resources/views/partials/ap/index.blade.php
+++ resources/views/partials/ap/index.blade.php
@@ -7,7 +7,12 @@
       <input type="text"
         class="form-control" name="authorized_price" id="authorized_price" value="{{ $cs['pivot']['authorized_price'] ?? '0.00'}}" aria-describedby="helpId" placeholder="">
     </div>
-    {{ $cs['pivot']['date_authorized'] ?? 'Not Authorized' }}<br /> 
+    <div class="form-group">
+      <label for="">Date Authorized</label>
+      <input type="date"
+        class="form-control" name="date_authorized" id="date_authorized" value="{{ $cs['pivot']['date_authorized'] ?? 'Not Authorized' }}" aria-describedby="helpId" placeholder="">
+      <small id="helpId" class="form-text text-muted">Help text</small>
+    </div>
 @endforeach
 <hr>
 @endforeach
