d5e79bb0fe1f8290e9423117314ea29c0ee13359
glcordon <glcordon@gmail.com>
2019-10-21 07:53:55 -0400

    Added delete function

--- resources/views/partials/ap/output.blade.php
+++ resources/views/partials/ap/output.blade.php
@@ -30,8 +30,8 @@
             @foreach($client['service'] as $cs)
         <tr>
             <td>{{ $client['last'] }}, {{ $client['first'] }}</td>
-            <td>{{ $cs['pivot']['authorized_price'] ?? '0.00'}}</td>
-            <td>${{ $cs['short_code'] ?? ''}}</td>
+            <td> ${{ $cs['pivot']['authorized_price'] ?? '0.00'}}</td>
+            <td> {{ $cs['short_code'] ?? ''}}</td>
             <td></td>
             <td></td>
             <td></td>
