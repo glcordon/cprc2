4d4437735c64cd19b164241abc3d9467373f61b4
glcordon <glcordon@gmail.com>
2019-10-20 23:18:12 -0400

    Added delete function

--- resources/views/partials/ap/index.blade.php
+++ resources/views/partials/ap/index.blade.php
@@ -45,8 +45,8 @@
                 <input type="date" name="date_authorized" id="date_authorized" value="{{ $cs['pivot']['date_authorized'] ?? 'Not Authorized' }}" aria-describedby="helpId" placeholder="">
             </td>
             <td colspan="4">
-                <button id="updateClient" service-id="{{ $cs['pivot']['service_id'] }}" data-id="{{ $cs['pivot']['id']}}">Update</button>
-                <button data-id="{{ $cs['pivot']['id']}}" id="deleteThis" class="btn btn-danger">X</button>
+                <button id="updateClient" service-id="{{ $cs['pivot']['service_id'] }}" data-id="{{ $cs['pivot']['id']}}" class="btn btn-sm">Update</button>
+                <button data-id="{{ $cs['pivot']['id']}}" id="deleteThis" class="btn btn-sm btn-danger">X</button>
             </td>
         </tr>
     
