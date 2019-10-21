a9a29cf31cfddcd34ad3ab912651e2cee61c4447
glcordon <glcordon@gmail.com>
2019-10-20 21:35:39 -0400

    turned on timestamps

--- resources/views/partials/ap/index.blade.php
+++ resources/views/partials/ap/index.blade.php
@@ -38,14 +38,14 @@
 <script>
     $(document).ready(function(){
         $(document).on('click','#updateClient', function(){
-            let client_id = $(this).attr('data-id')
+            let pivot_id = $(this).attr('data-id')
             let service_id = $(this).attr('service-id')
             var authorized_price = $(this).parent().parent().find('#authorized_price').val()
             var date_authorized = $(this).parent().parent().find('#date_authorized').val()
             var token = "{{ @csrf_token() }}"
             axios.post('/ap/update-service', {
                 _token:token,
-                client_id: client_id,
+                pivot_id: pivot_id,
                 service_id: service_id,
                 authorized_price:authorized_price,
                 date_authorized:date_authorized,
