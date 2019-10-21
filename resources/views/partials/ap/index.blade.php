239a0327b01cfc5ce26483b0db4a3f7f870f6411
glcordon <glcordon@gmail.com>
2019-10-20 23:23:43 -0400

    Added delete function

--- resources/views/partials/ap/index.blade.php
+++ resources/views/partials/ap/index.blade.php
@@ -30,7 +30,7 @@
     {{ Carbon\Carbon::create()->month($thisDate)->startOfMonth()->format('m/d/Y') }} - {{ Carbon\Carbon::create()->month($thisDate)->endOfMonth()->format('m/d/Y')  }}<br>
     <br>
 </div>
-<button class="btn-raised">Export</button>
+<button class="btn-raised mr-0">Export</button>
 @foreach($clientData as $client)
 <h4>{{ $client['first'] }}, {{ $client['last'] }}</h4>
 <table class="table table-sm table-striped">
