3ea5e93cdfdc83dc040e49f5a2a6cb3a7073eb1b
glcordon <glcordon@gmail.com>
2019-10-20 20:40:38 -0400

    testing update function

--- app/Http/Controllers/AccountsPayableController.php
+++ app/Http/Controllers/AccountsPayableController.php
@@ -53,7 +53,7 @@ class AccountsPayableController extends Controller
     public function updateService(Request $request)
     {
         $client = Client::find($request->client_id);
-        $client->services()->sync([$request->service_id =>['date_authorized'=>$request->date_authorized, 'authorized_price'=>$request->authorized_price]]);
+        $client->services()->attach([$request->service_id =>['date_authorized'=>$request->date_authorized, 'authorized_price'=>$request->authorized_price]]);
         return $client->services;
     }
 
