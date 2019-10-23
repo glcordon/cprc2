d87d62772f622303463e7b4e5980c6fa700a071b
glcordon <glcordon@gmail.com>
2019-10-23 19:58:57 -0400

    Added grand total

--- app/Http/Controllers/AccountsPayableController.php
+++ app/Http/Controllers/AccountsPayableController.php
@@ -28,6 +28,7 @@ class AccountsPayableController extends Controller
         $clients = Client::whereHas('services', function ($query) use($thisDate) {
             $query->whereMonth('date_authorized','=', $thisDate);
         })->with('services')->get();
+        dd($clients->services);
         $clientData = $clients->map(function($x){ 
             $serviceData = collect($x->services)->map(function($y){
                 $pd = collect($y->pivot)->toArray();
