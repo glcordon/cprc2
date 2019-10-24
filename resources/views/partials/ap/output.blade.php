571446fcbae7130bfb339938f998c04db6678454
glcordon <glcordon@gmail.com>
2019-10-24 07:53:53 -0400

    Added grand total

--- resources/views/partials/ap/output.blade.php
+++ resources/views/partials/ap/output.blade.php
@@ -9,12 +9,12 @@
 <table style="margin:0 auto; width:75%" cellspacing=0>
     <thead>
         <th style="border:1px solid black; padding:10px;">Client Name</th>
-        <th style="border:1px solid black; padding:10px;" colspan="2">Contract Services
+        <td style="border:1px solid black; padding:10px;" colspan="2"><strong> Services</strong>
             <br> 
             H=Housing<br />	
             T=Transportation (Private)	<br />
             C=Child Care	<br />
-        </th>
+        </td>
         <th style="border:1px solid black; padding:10px;" colspan="2">Supplies</th>
         <th style="border:1px solid black; padding:10px;" colspan="2">Training</th>
         <th style="border:1px solid black; padding:10px;">Other</th>
