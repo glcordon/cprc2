5d38af022a5219aa6ec67665247e164e765966a4
glcordon <glcordon@gmail.com>
2019-10-24 10:00:07 -0400

    Added grand total

--- resources/views/partials/clients/client-contact.blade.php
+++ resources/views/partials/clients/client-contact.blade.php
@@ -135,7 +135,7 @@
                             +Touch Point
                         </button>   
                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#servicesModal">
-                            +Service
+                            +AP
                         </button>   
                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addJobModal">
                                 +Job Data
