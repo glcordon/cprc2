01e51c30ea15e34f14c5ba09e77acbd421980a33
glcordon <glcordon@gmail.com>
2019-10-24 10:06:13 -0400

    Added grand total

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -29,7 +29,7 @@
               <li class="nav-item">
                   @if(\Auth::user())
                     <a class="nav-link" href="#"  data-toggle="modal" data-target="#date_modal2">Participation Report</a>
-                    <a class="nav-link" href="/ap">Participation Report</a>
+                    <a class="nav-link" href="/ap">AP</a>
                     
                   @endif
                 </li>
