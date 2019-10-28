1622629351b6cfdad3f98fb4cf4585e6a4ca164e
glcordon <glcordon@gmail.com>
2019-10-28 01:25:05 -0400

    Added grand total

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -232,7 +232,7 @@ class ClientController extends Controller
     {
         if($request)
         {
-            dd($request->image_file);
+            dd($request->all());
         $client = Client::find($request->client_id);
           ClientService::updateOrCreate([
             'service_id' => $request->service_id, 
