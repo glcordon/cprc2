0e4f369a9ef855ff0a313c995d04feb105753271
glcordon <glcordon@gmail.com>
2019-10-28 19:55:51 -0400

    Added file upload via axios

--- resources/views/partials/clients/client-contact.blade.php
+++ resources/views/partials/clients/client-contact.blade.php
@@ -104,7 +104,7 @@
               <div class="service_div">
 
                 @foreach($services as $srv)
-                @dd($srv)
+                @dump($srv)
                      <a href="#"><h5 class="card-title"> {{ $srv['service_name'] ?? '' }}</h5></a>
                      <p>{{ $srv->pivot->date_authorized }}</p>
                      @if($srv->file_url) 
