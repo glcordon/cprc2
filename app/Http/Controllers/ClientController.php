5dde546be09f0243b3889ee6fb771402c639cf94
glcordon <glcordon@gmail.com>
2019-10-28 20:33:00 -0400

    Added file upload via axios

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -130,7 +130,6 @@ class ClientController extends Controller
         //
         $clients = Client::find($id);
         $services = $clients->services;
-        dd($services);
         $additional_service = Services::where('service_type', '!=', 'referred')->get();
         $allServices = collect($additional_service->toArray());
         $otherServices = $additional_service->diff($services)->sortBy('service_name')->toArray();
