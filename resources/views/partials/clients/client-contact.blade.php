d64e774f7fddd7085b79e773939f52895dbab4f3
glcordon <glcordon@gmail.com>
2019-10-31 00:43:58 -0400

    Added modal trigger for service tag click

--- resources/views/partials/clients/client-contact.blade.php
+++ resources/views/partials/clients/client-contact.blade.php
@@ -103,7 +103,7 @@
               <div class="service_div">
 
                 @foreach($services as $srv)
-                     <a href="#"><span class="card-title"> {{ $srv['service_name'] ?? '' }}</span> </a>
+                     <a href="#" id="this_service" data-id="{{ $srv['id'] }}"><span class="card-title"> {{ $srv['service_name'] ?? '' }}</span> </a>
                      @if($srv->pivot->file_url) 
                      <a href="{{ '/get-file/'.$clients->id.'/'.$srv->pivot->file_url }}"><i class="fas fa-file-export"></i></a> 
                      @endif
@@ -534,6 +534,12 @@
               this_div.parent().fadeOut().remove()
             })
         })
+        $(document).on('click', '#this_service', function(e){
+          e.preventDefault()
+          $('#servicesModal').modal.show()
+          
+        })
+        
     });
 
       //  });
