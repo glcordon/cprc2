5dd0df215055f9fad75ae68a4d7abc531d1dde94
glcordon <glcordon@gmail.com>
2019-11-06 07:44:47 -0500

    Added function to update services

--- resources/views/partials/clients/client-contact.blade.php
+++ resources/views/partials/clients/client-contact.blade.php
@@ -556,7 +556,7 @@
           .then(response =>{
             console.log(response.data)
             $('#servicesModal').find('input#service_date').val(moment(response.data['date_authorized']).format('YYYY-MM-DD'))
-            $('#servicesModal').find('#amount_authorized').val(response.data['amount_authorized'])
+            $('#servicesModal').find('#amount_authorized').val(response.data['authorized_price'])
             $('#servicesModal').find('input#service_notes').val(response.data['notes'])
             $('#servicesModal').find('#saveServices').attr('data-btn-type', 'edit');
             $('#servicesModal').find('#saveServices').attr('data-id', response.data['id']);
