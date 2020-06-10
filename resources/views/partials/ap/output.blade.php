3c884d3c633bf73e654e3445861cb49e957350e4
glcordon <glcordon@gmail.com>
2020-06-09 22:19:55 -0400

    added logo to the site

--- resources/views/partials/ap/output.blade.php
+++ resources/views/partials/ap/output.blade.php
@@ -56,7 +56,7 @@
             <td style="padding:5px; border:1px solid #999;">{{ $client['last'] }}, {{ $client['first'] }}</td>
             <td style="border:1px solid #999; padding:0;" colspan="2" valign="top">
                 @if(array_key_exists('contract', $client['service']->toArray()))
-                    <table style="width:100%;" cellspacing="0" cellpadding='0'>
+                    <table style="width:100%; margin:-1px;" cellspacing="0" cellpadding='0'>
                         @foreach($client['service']['contract'] as $contract)
                         <tr>
                             <td width="60%" style="padding:4px; border:1px solid #999">${{ $contract['pivot']['authorized_price'] }}</td>
