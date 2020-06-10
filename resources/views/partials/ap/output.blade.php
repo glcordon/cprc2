d76a332ac42e851ae8f920173bccb544ad982d4b
glcordon <glcordon@gmail.com>
2020-06-09 22:08:20 -0400

    added logo to the site

--- resources/views/partials/ap/output.blade.php
+++ resources/views/partials/ap/output.blade.php
@@ -107,7 +107,10 @@
       <tr><td style="padding:5px; border:1px solid #999; font-weight:bold">Total</td><td colspan="8" valign="top" style="padding:5px; border:1px solid #999;text-align:right; font-weight:bold">${{ $grandTotal ?? '0.00' }}</td></tr>
     </tfoot>
 </table>
-<img src="{{ URL::to('/') }}/img//logo.jpg" alt="">
+<img src="{{ URL::to('/') }}/img//logo.jpg" width="300px" alt=""><br>
+503 Guion St<br />
+New Bern, NC 28561
+
 @endauth
 
     {{--  {{ $cs['service_name'] }} 
