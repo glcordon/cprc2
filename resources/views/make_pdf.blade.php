9f13e5741d6c0910c2c6471046579100d9a0490c
glcordon <glcordon@gmail.com>
2020-08-14 01:28:59 -0400

    added colors to form

--- resources/views/make_pdf.blade.php
+++ resources/views/make_pdf.blade.php
@@ -27,7 +27,7 @@
 		</div>
 		<div class="col-8" style="text-align:center;">
 			<h1>North Carolina Local Reentry Council</h1>
-			<h2>Monthly Progress Report</h2>
+			<h2>Monthly Data Collection</h2>
 			<h3>Due by 15th of each Month</h3>
 		</div>
 	</div>
